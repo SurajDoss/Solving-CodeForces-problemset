e256affec4558974f25c23a1289ed53fade00404
Suraj Doss <suraj_sebastien@yahoo.com>
2021-01-24 13:58:29 +0530

    Add files via upload

---
+++ 281A Word Capitalization.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main(){
+string s;
+cin>>s;
+//to capitalize first letter.
+s[0] = towupper(s[0]);//changes the value to uppercase
+cout<< s<<endl;
+return 0;
+}
