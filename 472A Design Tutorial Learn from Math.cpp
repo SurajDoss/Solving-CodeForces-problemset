0ce29900280e044e6dadb8efc1fce1eed6b82e8f
Suraj Doss <suraj_sebastien@yahoo.com>
2021-02-02 20:08:22 +0530

    Add files via upload

---
+++ 472A Design Tutorial Learn from Math.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+int value;
+
+cin>> value;
+
+if(value%2==0){
+    cout<<value - 4<< " 4";
+}else {
+    cout << value - 9 << " 9";
+}
+}
